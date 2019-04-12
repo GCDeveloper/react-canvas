49803f83e842603a004a1d3500b5724208eb4ee7
George O. E. Campbell <gcampbell@expresskcs.com>
2019-04-12 14:04:59 +0100

    b

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import OpenSimplexNoise from 'open-simplex-noise';
-let mouseRadius = 250;
-let dotRepelRadius = 50;
+let mouseRadius = 60;
+let dotRepelRadius = 20;
 let dotRadius = 2.5;
 let dotsToSpawn = 100;
 let mx = 0;
 let my = 0;
 let ox = 0;
 let oy = 0;
+let frameDots = 0;
 class App extends Component {
   constructor(){
     super();
@@ -22,15 +23,172 @@ class App extends Component {
     this.frame = 0;
     this.startCs = 48;
     this.nMaps = 14;
-
+    this.dots = [];
     this.openSimplex = new OpenSimplexNoise(Date.now());
   }
   reset = (n = 1) => {
     this.startCs = Math.max(12, Math.floor(this.startCs*n));
     //this.nMaps = Math.floor(this.nMaps*n);
     this.frame = 0;
+    frameDots = 0;
     this.cs = this.createCs(this.startCs);
     this.noiseMap = this.mapNoise(this.nMaps);
+    this.initDots();
+  }
+  initDots = () => {
+    const w = this.w;
+    const h = this.h;
+    let dotsToSpawn = Math.floor(((w+h)/2)/10);
+    this.dots = [];
+    for(let i=0;i < dotsToSpawn;i++){
+      this.dots.push({
+        x: Math.random()*w,
+        y: Math.random()*h,
+        vx:Math.random()*2-1,
+        vy:Math.random()*2-1,
+        r: Math.random()*4+1
+      });
+    }
+  }
+  updateDots = (mx, my) => {
+    const w = this.w;
+    const h = this.h;
+    let xCount = 0;
+    let yCount = 0;
+    let frame = frameDots;
+    //const wh = w*h;
+    this.dots = this.dots.map(({x,y,vx,vy,r}, i) => {
+
+      //let distFromCenter = Math.sqrt(((w/2-x)**2)+((h/2-y)**2));
+      //move x,y of dot away from mouse
+      //let dist = Math.sqrt(((mx-x)**2)+((my-y)**2));
+      //let angle = Math.atan2(my-y, mx-x);
+      //if(dist < mouseRadius){
+      // x += Math.cos(angle)*(mouseRadius/dist);
+      // y += Math.sin(angle)*(mouseRadius/dist);
+       //dist = Math.sqrt(((mx-x)**2)+((my-y)**2));
+      //}
+      this.dots.forEach((d, j) => {
+        if(i !== j){
+          let dist = Math.sqrt(((d.x-x)**2)+((d.y-y)**2));
+          let angle = Math.atan2(d.y-y, d.x-x);
+          let xWarp = (r*d.r);
+          let yWarp = (r*d.r);
+          if(frame < 385+(i+j)){
+            if(dist < mouseRadius){
+              x -= Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/80));
+              y -= Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/80));
+            } else {
+              x += Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/80));
+              y += Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/80));
+            }
+          } else if(frame < 450+(i)){
+            mouseRadius += Math.abs(Math.sin(frame/(xWarp**2)));
+            //angle += Math.PI*(distFromCenter/wh);
+            angle += Math.PI*((frame-385)*0.01);
+            if(dist < mouseRadius){
+              x -= Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/xWarp));
+              y -= Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/yWarp));
+            } else {
+              x += Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/xWarp));
+              y += Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/yWarp));
+            }
+          } else if(frame < 1150+(i)){
+            mouseRadius = 250;
+            angle += Math.PI*((frame-675)*0.02);
+            if(dist < mouseRadius){
+              x -= Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/80));
+              y -= Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/80));
+            } else {
+              x += Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/80));
+              y += Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/80));
+            }
+          } else if(frame < 1865+(i)){
+            xWarp = 80;
+            yWarp = 80;
+            if(dist < mouseRadius){
+              x -= Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/xWarp));
+              y -= Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/yWarp));
+            } else {
+              x += Math.cos(angle)*(dotRepelRadius/dist)*Math.abs(Math.sin(frame/xWarp));
+              y += Math.sin(angle)*(dotRepelRadius/dist)*Math.abs(Math.cos(frame/yWarp));
+            }
+          } else if(frame < 4200+(i)){
+            if(frame === 1865+(i)){
+              vx = Math.cos(angle-Math.PI/2)*Math.random()*2;
+              vy = Math.sin(angle-Math.PI/2)*Math.random()*2;
+            }
+            //if(dist < mouseRadius*0.25){
+            //  vx -= Math.cos(angle)*(dotRepelRadius/dist);
+            //  vy -= Math.sin(angle)*(dotRepelRadius/dist);
+            //} else {
+              vx += (Math.cos(angle)*(dotRepelRadius/dist**(1.8)))*0.01;
+              vy += (Math.sin(angle)*(dotRepelRadius/dist**(1.8)))*0.01;
+              if(dist < r+d.r){
+                vx -= (Math.cos(angle)*(dotRepelRadius/dist**(1.8)))*0.1;
+                vy -= (Math.sin(angle)*(dotRepelRadius/dist**(1.8)))*0.1;
+                x -= Math.cos(angle)*(Math.abs(vx+vy)/2);
+                y -= Math.sin(angle)*(Math.abs(vx+vy)/2);
+                //vx *= 0.7;
+                //vy *= 0.7;
+                //d.vx = (d.vx+vx)/2;
+              //  vx = d.vx;
+              //  d.vy = (d.vy+vy)/2;
+              //  vy = d.vy;
+              //  vx -= (Math.cos(angle)*(dotRepelRadius/dist))/10;
+              //  vy -= (Math.sin(angle)*(dotRepelRadius/dist))/10;
+              //  vx *= 0.9;
+              //  vy *= 0.9;
+              }
+              if(Math.abs(vx)+Math.abs(vy) > 11){
+                vx *= 0.5;
+                vy *= 0.5;
+              }
+           //}
+
+         } else {
+           vx = 0;
+           vy = 0;
+           //frame = 0;
+           //console.log("frame just set to 0", frame);
+           if(dist < mouseRadius){
+             x -= Math.cos(angle)*(dotRepelRadius/dist);
+             y -= Math.sin(angle)*(dotRepelRadius/dist);
+           } else {
+             x += Math.cos(angle)*(dotRepelRadius/dist)*4;
+             y += Math.sin(angle)*(dotRepelRadius/dist)*4;
+           }
+           if(frame >= 4600+(i)){
+             frameDots = 0;
+             frame = 0;
+           }
+           //vx = 0;
+           //vy = 0;
+         }
+        }
+      });
+      if(frame >= 1865){
+        x += vx;
+        y += vy;
+      }
+      //vx *= 0.99;
+      //vy *= 0.99;
+      xCount += x;
+      yCount += y;
+      return {x,y,vx,vy,r}
+    });
+    ox = xCount/this.dots.length-w/2;
+    oy = yCount/this.dots.length-h/2;
+  }
+  drawDots = () => {
+    const w = this.w;
+    const h = this.h;
+    const ctx = this.ctx;
+
+    this.dots.forEach(({x,y,r}) => {
+      ctx.moveTo(x+r-ox, y-oy);
+      ctx.arc(x-ox, y-oy, r, 0, Math.PI*2);
+    });
   }
   createCs = (n) => {
     const { w, h } = this;
@@ -137,7 +295,7 @@ class App extends Component {
     //console.log("avg:", this.o);
     this.zoom = this.getZoom();
   }
-  draw(frame){
+  draw = (frame) => {
     const { ctx, zoom, w, h } = this;
 ctx.fillStyle='rgba(255,255,255,0.1)';
     //ctx.fillStyle = `rgba(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},0.1)`; // The closer to black the better
@@ -176,6 +334,7 @@ ctx.fillStyle='rgba(255,255,255,0.1)';
     //
     //
     // }
+    this.drawDots();
     ctx.closePath();
     ctx.fill();
       this.noiseMap[frame%this.nMaps] = this.mapNoise(1, frame)[0];
@@ -328,6 +487,7 @@ ctx.fillStyle='rgba(255,255,255,0.1)';
     //this.startCs++;
     if(this.allowLoop){
       this.t0 = performance.now();
+      this.updateDots();
       this.update(this.frame)
       //while(this.frame < 100){
         this.draw(this.frame);
@@ -350,6 +510,8 @@ ctx.fillStyle='rgba(255,255,255,0.1)';
   resize(w = 480,h = 480){
     this.w = Math.round(Math.max(w, 16));
     this.h = Math.round(Math.max(h, 16));
+    mouseRadius = this.w/10;
+    dotRepelRadius = this.w/20;
     this.canvasElement.width = this.w;
     this.canvasElement.height = this.h;
     const ww = window.innerWidth;
@@ -371,6 +533,7 @@ ctx.fillStyle='rgba(255,255,255,0.1)';
     this.ctx = this.canvasElement.getContext('2d');
     this.ctx.strokeStyle = "#FFFFFF";
     this.ctx.fillStyle = 'rgb(255,255,255)';
+    this.initDots();
     this.cs = this.createCs(this.startCs);
     this.noiseMap = this.mapNoise(this.nMaps);
     this.loop();
