8791f5d8661828240cbba4d67b2160edbb28aa5f
George <campbell.goe@gmail.com>
2019-04-20 17:42:34 +0100

    loop

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+		  window.webkitRequestAnimationFrame || 
+		  window.mozRequestAnimationFrame    || 
+		  window.oRequestAnimationFrame      || 
+		  window.msRequestAnimationFrame     || 
+		  function( callback ){
+			window.setTimeout(callback, 1000 / 30);//fallback to setTimeout
+		  };
+})();
 
 class App extends Component {
-  resize(){
-    this.w = window.innerWidth;
-    this.h = window.innerHeight;
-    this.canvasElement.width = this.w;
-    this.canvasElement.height = this.h;
-  }
   componentDidMount(){
     this.resize();
     window.addEventListener("resize", ()=>{
       this.resize();
     });
     this.ctx = this.canvasElement.getContext('2d');
-    const ctx = this.ctx;
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(this.w, this.h);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
-    ctx.closePath();
+    this.loop();
+  }
+  componentWillUnmount(){
+    window.removeEventListener("resize", ()=>{
+      this.resize();
+    });
   }
   resize(){
     this.w = window.innerWidth;
     this.h = window.innerHeight;
     this.canvasElement.width = this.w;
     this.canvasElement.height = this.h;
+  }
+  update(){
+
+  }
+  draw(){
+    const { ctx, w, h } = this;
+    ctx.clearRect(0,0,w,h);
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(w, h);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    ctx.closePath();
+  }
+  loop(){
+    this.update();
+    this.draw();
+    window.requestAnimFrame(this.loop.bind(this));
   }
   render() {
     return (
