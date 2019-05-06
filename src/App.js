b4a8a2beb486ad8afb9f6592dc87eab8d73bee34
George Campbell <campbell.goe@gmail.com>
2019-05-06 16:55:50 +0100

    wavey

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ let mx = 0;
 let my = 0;
 let ox = 0;
 let oy = 0;
+let tx, ty;
 let frame = 0;
 class App extends Component {
   resize(){
@@ -12,6 +13,8 @@ class App extends Component {
     this.h = window.innerHeight;
     this.canvasElement.width = this.w;
     this.canvasElement.height = this.h;
+ox = -this.w/2;
+oy = -this.h/2;
   }
   init(){
     const w = this.w;
@@ -23,11 +26,14 @@ class App extends Component {
         y: oy,
         vx:0,
         vy:0,
-        r: 1
+        r: 2
       });
     }
   }
   update(mx, my){
+    let bnd = v => Math.abs(Math.min(v,2))-1;
+    tx = bnd(Math.cos(frame/100)+Math.cos(frame/50));
+    ty = bnd(Math.sin(frame/50)*Math.sin(frame/50));
     const w = this.w;
     const h = this.h;
     let xCount = 0;
@@ -35,23 +41,28 @@ class App extends Component {
     //const wh = w*h;
     console.log(frame);
     dots = dots.map(({x,y,vx,vy,r}, i) => {
-      xCount += x;
-      yCount += y;
+     // xCount += x;
+     // yCount += y;
+
+      x = tx*w/4;
+      y = ty*h/4;
       return {x,y,vx,vy,r}
     });
-    ox = xCount/dots.length-w/2;
-    oy = yCount/dots.length-h/2;
+//    ox = xCount/dots.length-w/2;
+//    oy = yCount/dots.length-h/2;
   }
   draw(){
     const w = this.w;
     const h = this.h;
     const ctx = this.ctx;
-    ctx.fillStyle='rgba(0,0,0,0.05)';
-    ctx.fillRect(0,0,w,h);
+    if(frame % 10 === 0) {
+      ctx.fillStyle='rgba(0,0,0,0.05)';
+      ctx.fillRect(0,0,w,h);
+    }
     ctx.beginPath();
     dots.forEach(({x,y,r}) => {
-      ctx.moveTo(x+r-ox, y-oy);
-      ctx.arc(x-ox, y-oy, r, 0, Math.PI*2);
+      ctx.moveTo(Math.round(x+r-ox), Math.round(y-oy));
+      ctx.arc(Math.round(x-ox), Math.round(y-oy), r, 0, Math.PI*2);
     });
     ctx.closePath();
     ctx.fillStyle = 'white';
